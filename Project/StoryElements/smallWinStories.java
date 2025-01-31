759048293df21e40328a0a411b87eff4ac7330b0
Noah Cosamano <njc@g.rit.edu>
2025-01-31 17:00:15 -0500

    updated

---
+++ Project/StoryElements/smallWinStories.java
@@ -0,0 +1,46 @@
+package Project.StoryElements;
+
+public class smallWinStories {
+    String d7n1 = "You ran into a wall... But it made you tougher!\nDefense +7%";
+    String d7n2 = "You remembered getting bullied in middle school... What doesn't " +
+                "kill you makes you stronger!\nDefense +7%";
+    String d7n3 = "You started taking Aikido classes with hopes in becoming a sensei." +
+                "\nDefense +7%";
+    String d5n1 = "Your Mom yelled at you for not calling her back. It's tough love!" +
+                "\nDefense +5%";
+    String d5n2 = "You put on a couple of pounds over Thanksgiving. But it's " +
+                "But it's Thanksgiving!\nDefense +5%";
+    String d5n3 = "You've started ab-strengthening! With a lead pipe...\nDefense +5%";
+    String d10n1 = "Your Grandma gave you a pep talk about you being tough. See, " +
+    "somebody still believes in you!\nDefense +10%";
+
+
+    String h7n1 = "You started drinking protein shakes every morning.\nHealth +7%";
+    String h7n2 = "You switched from normal water, to alkaline water. Science!\nHealth +7%";
+    String h7n3 = "You saw a Reddit post about using dinner plates as body armor. Why not! " +
+                "\nHealth +7%";
+    String h5n1 = "You started wearing fuzzy socks to keep yourself warm from this cold-" +
+                "hearted game developer.\nHealth +5%";
+    String h5n2 = "You tried to meditate, but ended up napping for 3 hours instead. Still counts!" +
+                "\nHealth +5%";
+    String h5n3 = "You discovered that a little extra sleep goes a long way. But you’re still a " +
+                "little late to everything.\nHealth +5%";
+    String h10n1 = "You finally learned to drink water like a responsible adult, instead of waiting " +
+                "until you’re dehydrated and miserable.\nHealth +10%";
+
+    
+    String a7n1 = "You discovered the power of a perfectly timed elbow jab. Looks like that ninja " +
+                "movie marathon paid off!\nAttack +7%";
+    String a7n2 = "You’ve been practicing your battle cries. Now you sound terrifying... or at least " +
+                "confused.\nAttack +7%";
+    String a7n3 = "You learned the secret to speed attacks: caffeine. Lots and lots of caffeine.\n" +
+                "Attack +7%";
+    String a5n1 = "You learned to throw punches while watching action movies. Too bad they weren’t " +
+                "aiming at anything.\nAttack +5%";
+    String a5n2 = "You slapped a mosquito and felt like a champion. Who needs swords when you’ve got " +
+                "hands?\nAttack +5%";
+    String a5n3 = "You finally perfected your ninja kick. Too bad it was against a pile of laundry.\n" +
+                "Attack +5%";
+    String a10n1 = "You trained by shadowboxing your reflection. You won... mostly.\n" +
+                "Attack +10%";
+}
